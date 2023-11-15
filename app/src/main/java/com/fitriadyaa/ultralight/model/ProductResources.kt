d53f7687d660f189407be2736b6833b992299651
fitriadyaa <fitriadyaa@jecha-air.local>
2023-11-15 14:11:08 +0700

    adding data

--- app/src/main/java/com/fitriadyaa/ultralight/model/ProductResources.kt
+++ app/src/main/java/com/fitriadyaa/ultralight/model/ProductResources.kt
@@ -7,5 +7,13 @@ object ProductResources {
         Product(1, R.drawable.mahagane, "Mahagane eSQiu 25L Ecopak", 380, "Frameless UL bacpack (custom req.)", "https://shopee.co.id/Backpack-Mahagane-25L-Esqiu-i.162451568.8951220640"),
         Product(2, R.drawable.dhaulagiri, "Dhaulagiri Foldable Pole 103", 215, "Trekking poles, tarp tent pole", "https://shopee.co.id/Dhaulagiri-Dhaulagiri-Trekking-Pole-103-Trekking-Pole-Lipat-i.42345838.3143315255?sp_atk=e6c3b6c1-c4ca-4faf-b95c-526d9bc63cd8&xptdk=e6c3b6c1-c4ca-4faf-b95c-526d9bc63cd8"),
         Product(3, R.drawable.big_adventure, "Big Adventure Sembalun 1", 70, "Alumunium foil bubble wrap (cut to torso 50x100cm)", "https://www.tokopedia.com/bigadventureindo/bigadventure-sembalun-single-aluminium-matras-bubble-pad"),
+        Product(4, R.drawable.agyer, "AGrey Tarp Tent 1P", 705, "Outer + inner half-mesh, without stuffsack", "https://shopee.co.id/Agrey-1P-UL-Tent-i.162451568.15227593031"),
+        Product(5, R.drawable.norge,  "Norgee Dry Sack 25L", 110, "Waterproof packliner 25L, camp storage bag, washing bucket", "https://www.tokopedia.com/yamitala-1/dry-sack-norgee-kuning-25-liter"),
+        Product(6, R.drawable.meikha,  "Meikhashop Ekalitas 300L", 536,   "Sleeping bag FP600 FW300 17⁰-2⁰C range", "https://www.tokopedia.com/meikhashop/sb-bulang-sleeping-bag-bulu-angsa-ekalitas300"),
+        Product(7, R.drawable.shemagh,  "Shemagh/Kaffiyeh", 118,   "Hiking buff, head cover, sajadah, towel, pillow", "https://www.tokopedia.com/alghifariwear/sorban-hitam-tactical-sorban-arab-sorban-palestina-segi-empat-random?extParam=ivf%3Dfalse&src=topads"),
+        Product(8, R.drawable.hiking,   "Pinnacle Trex", 300,    "Hiking/daily pants, light, stretch, quick-dry", "https://www.pinnaclepro.id/produk/pinnacle-trex-pant-dark-olive/"),
+        Product(9, R.drawable.kimanjaro,   "Kilimanjaro Polar Gloves", 48,    "Camp or cold/summit gloves", "https://www.tokopedia.com/kilimanjaro-adv/sarung-tangan-polar-gunung-outdoor-motor-kilimanjaro-biru"),
+        Product(10, R.drawable.merapi,   "Merapi Mountain Quarles", 70,     "Camp Ecodown down jacket + stuffsack", "http://www.store.merapimountain.com/2021/11/17/quarles-ecodown-jacket/"),
+        Product(11, R.drawable.mayko,    "Meyko TR-42", 302,      "Ultralight barefoot sandals, trail variant", "https://www.tokopedia.com/meykosandals/meyko-sandal-vegan-tr-42"),
     )
 }
